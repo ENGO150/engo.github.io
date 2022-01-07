4554415028616f281beb98b50244b0a7dae7f1d5
ENGO150 <v.smejkal06@gmail.com>
2022-01-07 19:16:04 +0100

    added eastereggs

---
+++ script.js
@@ -0,0 +1,9 @@
+function videoFunction()
+{
+    document.getElementsByClassName("video")[0].style.display = "table";
+}
+
+function musicFunction()
+{
+    document.getElementsByClassName("music")[0].style.display = "table";
+}
