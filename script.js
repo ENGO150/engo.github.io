c48e00d9765274449c80512a1a264f388dba358a
ENGO150 <v.smejkal06@gmail.com>
2022-01-12 19:51:18 +0100

    added easteregg info

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var videoDisplay = false;
 var musicDisplay = false;
 
+var easterEggNumber = 4;
+
 function videoFunction()
 {
     var buffer;
@@ -33,4 +35,14 @@ function musicFunction()
     document.getElementsByClassName("music")[0].style.display = buffer;
 
     musicDisplay = !musicDisplay;
+}
+
+function showInfo()
+{
+    document.getElementsByClassName("hidden-text")[0].innerHTML = "lol this page contains " + easterEggNumber + " easterggs...";
+
+    setTimeout(function ()
+    {
+        document.getElementsByClassName("hidden-text")[0].innerHTML = "";
+    }, 5000);
 }
